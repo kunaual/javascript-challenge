dd1c74bd700b4eecb7c28e86d35f4e1e5b3014e9
kunaual <aekunau@gmail.com>
2021-04-05 19:35:11 -0500

    commiting initial folders

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,13 @@
+// from data.js
+var tableData = data;
+
+//
+console.log("Loaded app.js including tableData");
+count=0;
+tableData.forEach((thing) => {
+    console.log(thing);
+    // count+=1;
+    // if (count === 3) { 
+    //     break; 
+    // }
+})
