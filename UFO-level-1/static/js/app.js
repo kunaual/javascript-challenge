ee817af77aa3803380999a1bb007e157efcd8c58
kunaual <aekunau@gmail.com>
2021-04-10 16:17:14 -0500

    added click event and select to get filter date value

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,13 +1,35 @@
+console.log("app.js loaded!")
 // from data.js
 var tableData = data;
+var button = d3.select("#filter-btn");
+//var datefield = d3.select("#datetime");
+button.on("click", filterClick);
 
-//
-console.log("Loaded app.js including tableData");
+//console.log(datefield);
+
+function filterClick() {
+    console.log("filter button was clicked!");
+
+    var dateValue = d3.select("#datetime").node().value;
+
+    //get data from the date field
+    console.log(dateValue);
+  
+    // We can use d3 to see the object that dispatched the event
+    console.log(d3.event.target);
+  }
+
 count=0;
 tableData.forEach((thing) => {
-    console.log(thing);
+  //  console.log(thing);
     // count+=1;
     // if (count === 3) { 
     //     break; 
     // }
 })
+// function filterData(event){
+//     var inputDate = d3.event.target.value;
+//     console.log("in filterData")
+// };
+
+//datefield.on("change", filterClick)
